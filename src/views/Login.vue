264741ac49dfc685bc1d871df0addee1996db658
zjcomeon <159109799@qq.com>
2019-07-26 23:38:25 +0800

    添加退出

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,10 +5,10 @@
       <div class="logo">E-SOP</div>
     </div>
     <div class="form-item username">
-      <input type="text"/>
+      <input type="text" v-model="username"/>
     </div>
     <div class="form-item password">
-      <input type="password"/>
+      <input type="password" v-model="password"/>
     </div>
     <div class="form-item loginBtn" @click="loginApp()">
     </div>
@@ -19,9 +19,17 @@
 <script>
 export default {
   name: 'login',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
   methods: {
     loginApp () {
-      this.$router.push({ path: '/task/list' })
+      if (this.username === 'admin' && this.password === '123456') {
+        this.$router.push({ path: '/task/list' })
+      }
     }
   }
 }
--- src/views/My.vue
+++ src/views/My.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="my-center">
-    <div class="my">
-    </div>
+    <div class="my"></div>
+    <div class="logout" @click="logout()"></div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'my',
+  methods: {
+    logout () {
+      this.$router.push({ path: '/' })
+    }
+  }
+}
+</script>
